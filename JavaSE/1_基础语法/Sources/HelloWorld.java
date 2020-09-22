6285dedb482ea2546ea1c1f8f6ff95b634318a7d
jonahcoding <2548860066@qq.com>
2020-09-22 12:01:59 +0800

    Update

---
+++ JavaSE/1_基础语法/Sources/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello Everyone");
+	}
+}
+
+class HelloWorldSecond {
+	public static void main(String[] args){
+		System.out.println("Welcome!");
+	}
+}
