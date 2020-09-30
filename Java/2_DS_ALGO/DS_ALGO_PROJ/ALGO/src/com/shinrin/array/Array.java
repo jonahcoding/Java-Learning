d7d41dd3aea90808fb53dd05c346a6319aeebadb
jonahcoding <2548860066@qq.com>
2020-09-30 17:28:20 +0800

    update

--- Java/2_DS_ALGO/DS_ALGO_PROJ/ALGO/src/com/shinrin/array/Array.java
+++ Java/2_DS_ALGO/DS_ALGO_PROJ/ALGO/src/com/shinrin/array/Array.java
@@ -2,47 +2,47 @@ package com.shinrin.array;
 
 public class Array {
     private int initialLen = 16;
-    private int size = 0;
-    private int capacity;
+    private int m_size = 0;
+    private int m_capacity;
     private int[] m_data;
     private double expandFactor = 0.75;
 
     public Array(){
-        capacity = initialLen;
-        m_data = new int[capacity];
+        m_capacity = initialLen;
+        m_data = new int[m_capacity];
     }
 
     public Array(int len){
         if (len <= 0){
-            capacity = initialLen;
+            m_capacity = initialLen;
         }else {
-            capacity = len;
+            m_capacity = len;
         }
-        m_data = new int[capacity];
+        m_data = new int[m_capacity];
     }
 
     public void resize(int len){
         int[] resizeArr = new int[len];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < m_size; i++) {
             resizeArr[i] = m_data[i];
         }
         m_data = resizeArr;
-        capacity = len;
+        m_capacity = len;
     }
 
     public void add(int index, int num){
-        if (index < 0 || index > size) {
+        if (index < 0 || index > m_size) {
             System.out.println("添加位置非法！");
             return;
         }
-        if (size >= capacity * expandFactor) {
-            resize(capacity * 2);
+        if (m_size >= m_capacity * expandFactor) {
+            resize(m_capacity * 2);
         }
-        for (int i = size; i > index; i--) {
+        for (int i = m_size; i > index; i--) {
             m_data[i] = m_data[i - 1];
         }
         m_data[index] = num;
-        size++;
+        m_size++;
     }
 
     public void addFirst(int num){
@@ -50,27 +50,27 @@ public class Array {
     }
 
     public void addLast(int num){
-        if (size >= capacity){
-            resize(capacity * 2);
+        if (m_size >= m_capacity){
+            resize(m_capacity * 2);
         }
-        m_data[size] = num;
-        size++;
+        m_data[m_size] = num;
+        m_size++;
     }
 
     public void remove(int index){
-        if (index < 0 || index >= size)
+        if (index < 0 || index >= m_size)
         {
             System.out.println("删除位置非法！");
             return;
         }
         int res = m_data[index];
-        size--;
-        for (int i = index; i < size; i++)
+        m_size--;
+        for (int i = index; i < m_size; i++)
         {
             m_data[i] = m_data[i + 1];
         }
-        if (size < capacity / 4) {
-            resize(capacity / 2);
+        if (m_size < m_capacity / 4) {
+            resize(m_capacity / 2);
         }
     }
 
@@ -79,34 +79,76 @@ public class Array {
     }
 
     public void removeLast(){
-        if (size == 0)
+        if (m_size == 0)
         {
             System.out.println("删除位置非法！");
             return;
         }
 //        remove(size);
-        size--;
-        if (size < capacity / 4)
+        m_size--;
+        if (m_size < m_capacity / 4)
         {
-            resize(capacity / 2);
+            resize(m_capacity / 2);
         }
     }
 
-    public int getCapacity(){
-        return capacity;
+    void removeElement(int num){
+        int index = find(num);
+        if (index != -1){
+            remove(index);
+        }
     }
 
-    public int getSize(){
-        return size;
+    int find(int num){
+        for (int i = 0; i < m_size; i++) {
+            if (m_data[i] == num){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void set(int index,int num){
+        if (index < 0 || index >= m_size)
+        {
+            System.out.println("位置非法！");
+            return;
+        }
+        m_data[index] = num;
+    }
+
+    int get(int index) {
+        return m_data[index];
+    }
+
+    boolean contains(int num){
+        for (int i = 0; i < m_size; i++) {
+            if (m_data[i] == num){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int capacity(){
+        return m_capacity;
+    }
+
+    public int size(){
+        return m_size;
+    }
+
+    boolean isEmpty(){
+        return m_size == 0;
     }
 
     public void print(){
         System.out.print("[Array]    ");
-        System.out.println("Capacity = " + capacity + "    Size = " + size);
+        System.out.println("Capacity = " + capacity() + "    Size = " + size());
         System.out.print("[");
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < m_size; i++) {
             System.out.print(m_data[i]);
-            if (i != size-1){
+            if (i != m_size -1){
                 System.out.print(",");
             }
         }
--- Java/2_DS_ALGO/DS_ALGO_PROJ/ALGO/src/com/shinrin/array/ArrayTest.java
+++ Java/2_DS_ALGO/DS_ALGO_PROJ/ALGO/src/com/shinrin/array/ArrayTest.java
@@ -5,14 +5,26 @@ public class ArrayTest {
         Array arr = new Array(16);
         for (int i = 0; i < 24; i++) {
             arr.add(i,i);
-//            System.out.println(i + " " + arr.getCapacity() + " " + arr.getSize());
         }
         arr.print();
         arr.addFirst(-1);
         arr.print();
+        arr.addLast(24);
+        arr.print();
+        arr.add(3,333);
+        arr.print();
+        arr.print();
+        arr.remove(3);
         arr.removeLast();
         arr.print();
-        arr.remove(2);
+        arr.removeFirst();
         arr.print();
+
+        System.out.println("数组为空？" + arr.isEmpty());
+        System.out.println("包含15？" + arr.contains(15));
+        System.out.println("获取索引位15：" + arr.get(15));
+        arr.set(15, 155);
+        arr.print();
+        System.out.println("获取索引位122：" + arr.get(122));
     }
 }
