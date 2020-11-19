df56a1c0d9e2f9630adb7406035afc2f11116097
jonahcoding <714092359@qq.com>
2020-11-20 00:45:26 +0800

    员工管理案例、员工展示

--- 06_微服务/02_SpringBoot/SpringBoot_PROJ/SpringBoot_03_Web/src/main/java/com/shinrin/config/MyMvcConfig.java
+++ 06_微服务/02_SpringBoot/SpringBoot_PROJ/SpringBoot_03_Web/src/main/java/com/shinrin/config/MyMvcConfig.java
@@ -27,6 +27,6 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/index.html", "/", "/user/login");
+                .excludePathPatterns("/index.html", "/", "/user/login","/emps");
     }
 }
---
+++ 06_微服务/SpringBoot_PROJ/SpringBoot_02_Config/src/main/java/com/shinrin/pojo/Dog.java
@@ -0,0 +1,16 @@
+package com.shinrin.pojo;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(value = "dog")
+public class Dog {
+    //@Value("阿黄")
+    private String firstName;
+    //@Value("18")
+    private Integer age;
+}
